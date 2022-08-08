aeb0eb91e34c5dafd084879b458ee78d3f6f437a
Wander Aquino <wander.aquino@gmail.com>
2022-08-08 01:08:17 -0300

    Include set for proxyFactory

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -3,7 +3,7 @@ class ProxyFactory {
         return new Proxy(targetObject, {
             get: function(target,prop) {
                 if(props.includes(prop)) {
-                    return function(handleUpdateModel, model = undefined) {
+                    return function(model = undefined) {
                         if(typeof target[prop] === typeof Function) {
                             console.log("Intercepted by method: ", prop);
                             target[prop](model || null);
@@ -16,7 +16,14 @@ class ProxyFactory {
                 console.log("Intercepted by property: ", prop);
 
                 return target[prop];
+            },
+            set: function (target, prop, value) {
+                if(props.includes(prop)) {
+                    target[prop] = value;
+                    handleUpdateModel(value);
+                    return value;
+                }
             }
         });
     }
-}
+ }
