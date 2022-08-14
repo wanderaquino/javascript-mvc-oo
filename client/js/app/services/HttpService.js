b5e1cb631605e02263c4e949966d04b069722b8c
Wander Aquino <wander.aquino@gmail.com>
2022-08-13 23:52:21 -0300

    Track new class HttpService

---
+++ client/js/app/services/HttpService.js
@@ -0,0 +1,18 @@
+class HttpService {
+    get(url) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
+            xhr.send();
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState === 4) {
+                    if(xhr.status === 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.responseText);
+                    }
+                }
+            }
+        })
+    }
+}
